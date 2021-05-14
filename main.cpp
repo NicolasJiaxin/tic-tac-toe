14f9f5884ab48c915253ce39c162b5bdd9e43b4f
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-05-14 11:52:36 -0400

    Better turn

--- main.cpp
+++ main.cpp
@@ -20,18 +20,10 @@ int main()
         cout << "Current state: " << endl;
         printGrid(grid);
         while(!isValid) {
-            cout << "Enter row (0-2):" << endl;
+            cout << "Enter row,column (e.g. 0,2) and hit enter:" << endl;
             cin >> row;
-            while (row > 2 || row < 0) {
-                cout << "INVALID. Enter row(0-2):" << endl;
-                cin >> row;
-            }
-            cout << "Enter column(0-2):" << endl;
+            cin.ignore(1);
             cin >> col;
-            while (col > 2 || col < 0) {
-                cout << "INVALID. Enter col(0-2):" << endl;
-                cin >> col;
-            }
             if(grid[row][col] == 0) {
                 grid[row][col] = player;
                 isValid = true;
@@ -52,5 +44,6 @@ int main()
         cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     }
     cout << "DRAW." << endl;
+    printGrid(grid);
     return 0;
 }
